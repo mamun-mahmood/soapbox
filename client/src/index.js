ae9fb3c861e9d15a2781e0b2fe2ec05860dd02b8
Harsh Mistry <hhrshmistry@gmail.com>
2021-08-19 16:40:15 +0530

    minor change

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
 import { hydrate, render } from 'react-dom';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
-const APP = (
+// const APP = (
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// const rootElement = document.getElementById('root');
+// if (rootElement.hasChildNodes()) {
+//   hydrate(APP, rootElement);
+// } else {
+//   render(APP, rootElement);
+// }
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-const rootElement = document.getElementById('root');
-if (rootElement.hasChildNodes()) {
-  hydrate(APP, rootElement);
-} else {
-  render(APP, rootElement);
-}
