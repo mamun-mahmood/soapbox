8367070951d86d4c63dc532b989f5dd268c9a27f
Aakash Bhardwaj <officialaakashbhardwaj@gmail.com>
2022-08-22 21:30:14 +0530

    deployed codes

---
+++ client/build/static/js/6.b82cbc9b.chunk.js
@@ -0,0 +1,2 @@
+webpackJsonp([6],{8721:function(e,n,a){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var t=a(0),l=a.n(t),r=a(87),u=a(28),c=a(68),i=a(144),m=a.n(i),o=a(145),d=m()({loader:function(){return a.e(9).then(a.bind(null,8735))},loading:function(){return l.a.createElement(o.a,null)}}),E=function(){return l.a.createElement(t.Fragment,null,l.a.createElement(u.a,null),l.a.createElement("div",{className:"main-body"},l.a.createElement(c.a,null),l.a.createElement(d,null),l.a.createElement(r.a,null)))};n.default=E}});
+//# sourceMappingURL=6.b82cbc9b.chunk.js.map
