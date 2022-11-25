e365de9d9d7e5057b94c75278cadecf5482b1cb2
mamun-mahmood <mamamun1999@gmail.com>
2022-11-25 16:15:43 +0600

    chathive logo change

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -26,7 +26,7 @@ import {
   AiOutlineMedium,
 } from "react-icons/ai";
 
-import chathive from "../assets/chathiveWText.svg";
+import chathive from "../assets/mchathive_logo.svg";
 import Notable from "../assets/RegularVerified.svg";
 import PremiumVerified from "../assets/PremiumVerified.svg";
 import CorporateVerified from "../assets/CorporateVerified.svg";
@@ -257,7 +257,7 @@ const Profile = ({
                 <button className="btn-edit-profile">
                   <Link to={`/chathive/${username}`}>
                     {" "}
-                    <img src={chathive} className="chativelogo" style={{width: "150px", height: "100%"}} />
+                    <img src={chathive} className="chativelogo"/>
                   </Link>
                 </button>
 
