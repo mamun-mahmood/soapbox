266e57ec29d94f1aa59ad4de894287bf22b42d03
mamun-mahmood <mamamun1999@gmail.com>
2023-02-06 22:31:38 +0600

    new build with embed ui fixed

---
+++ client/build/static/js/6.83f1aa0f.chunk.js
@@ -0,0 +1,2 @@
+webpackJsonp([6],{8756:function(e,n,a){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var t=a(0),l=a.n(t),r=a(92),u=a(28),c=a(71),i=a(157),m=a.n(i),o=a(158),d=m()({loader:function(){return a.e(9).then(a.bind(null,8770))},loading:function(){return l.a.createElement(o.a,null)}}),E=function(){return l.a.createElement(t.Fragment,null,l.a.createElement(u.a,null),l.a.createElement("div",{className:"main-body"},l.a.createElement(c.a,null),l.a.createElement(d,null),l.a.createElement(r.a,null)))};n.default=E}});
+//# sourceMappingURL=6.83f1aa0f.chunk.js.map
