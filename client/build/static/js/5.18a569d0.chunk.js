97efa77e056fc521a45ba25608b71e1abed3360d
mamun-mahmood <mamamun1999@gmail.com>
2023-02-21 15:07:10 +0600

    build for designation icos

---
+++ client/build/static/js/5.18a569d0.chunk.js
@@ -0,0 +1,2 @@
+webpackJsonp([5],{8761:function(e,n,a){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var t=a(0),l=a.n(t),r=a(91),u=a(157),c=a(28),i=a(71),m=a(156),o=a.n(m),d=o()({loader:function(){return a.e(8).then(a.bind(null,8773))},loading:function(){return l.a.createElement(u.a,null)}}),E=function(){return l.a.createElement(t.Fragment,null,l.a.createElement(c.a,null),l.a.createElement("div",{className:"main-body"},l.a.createElement(i.a,null),l.a.createElement(d,null),l.a.createElement(r.a,null)))};n.default=E}});
+//# sourceMappingURL=5.18a569d0.chunk.js.map
--- client/build/static/js/6.21e32f87.chunk.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([6],{8755:function(e,n,a){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var t=a(0),l=a.n(t),r=a(92),u=a(28),c=a(71),i=a(157),m=a.n(i),o=a(158),d=m()({loader:function(){return a.e(9).then(a.bind(null,8769))},loading:function(){return l.a.createElement(o.a,null)}}),E=function(){return l.a.createElement(t.Fragment,null,l.a.createElement(u.a,null),l.a.createElement("div",{className:"main-body"},l.a.createElement(c.a,null),l.a.createElement(d,null),l.a.createElement(r.a,null)))};n.default=E}});
-//# sourceMappingURL=6.21e32f87.chunk.js.map
